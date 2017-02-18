751c14ed234a548cc0377c4df02bfce29c052a32
Arif Akram Mohammed <m.arifakram@gmail.com>
2017-02-18 15:02:37 -0600

    adding http interceptors

--- js/app.js
+++ js/app.js
@@ -10,12 +10,39 @@ var scrumApp = angular.module(
         'scrumApp.shared'
 ])
 
-.config(function ($stateProvider, $urlRouterProvider, growlProvider) {
+.config(function ($stateProvider, $urlRouterProvider, growlProvider, $httpProvider) {
 
     $urlRouterProvider.otherwise('/login');
     growlProvider.globalTimeToLive(5000);
     growlProvider.globalPosition('bottom-right');
     growlProvider.globalDisableCountDown(true);
+
+    //interceptor for http calls
+    $httpProvider.interceptors.push('sbInterceptor');
+})
+
+//this method will intercept all http calls
+.factory('sbInterceptor', function ($rootScope, $q, $window, growl, $injector) {
+
+    return {
+        request: function (config) {
+            var SharedService = $injector.get('SharedService');
+            console.log('auth token : ', SharedService.getAuthToken());
+            console.log('Intercepted Service Call....Adding authToken to request....');
+            config.headers = config.headers || {};
+            if (SharedService.getAuthToken()) {
+                config.headers.Authorization = 'Bearer ' + SharedService.getAuthToken();
+            }
+            return config;
+        },
+        response: function (response) {
+            if (response.status === 401 || response.status === 403) {
+                // handle the case where the user is not authenticated
+                SharedService.showError('This operation cannot be performed as you are not authenticated');
+            }
+            return response || $q.when(response);
+        }
+    };
 })
 
 .controller('mainCtrl', ['$scope', '$uibModal', 'SharedService', function ($scope, $uibModal, SharedService) {
