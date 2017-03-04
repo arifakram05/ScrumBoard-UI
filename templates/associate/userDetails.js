554d4824e1a5c863132d3742f2b239f04b78ab67
Arif Akram Mohammed <m.arifakram@gmail.com>
2017-03-04 12:50:39 -0600

    feature - user can search another associate by id or name

--- templates/associate/userDetails.js
+++ templates/associate/userDetails.js
@@ -26,6 +26,22 @@ angular.module('scrumApp.userDetails', ['ui.router'])
         $scope.assignedProjects = SharedService.getProjectNames();
     }
 
+    //show other associates details
+    //search associates
+    $scope.search = function (searchText) {
+        var promise = SharedService.searchAssociates(searchText);
+        promise.then(function (result) {
+            console.log('got the result from searching associates :', result);
+            $scope.filteredAssociates = result.response;
+            //$scope.filteredAssociates = processSearchResults(result.response);
+        })
+            .catch(function (resError) {
+            console.log('search for associates failed :: ', resError);
+            //show failure message to the user
+            SharedService.showError(resError.message);
+        });
+    }
+
     //close modal
     $scope.closeUserDetailsModal = function () {
         console.log('closing user details modal');
